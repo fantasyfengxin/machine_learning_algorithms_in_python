76bd349bbf42b36100c9405aff29c67ec192145a
fx <fantasyfengxin@gmail.com>
2016-07-25 20:14:42 +0800

    a new function added

--- CH03/tress.py
+++ CH03/tress.py
@@ -24,4 +24,23 @@ def calcShannonEnt(dataSet):
 	for key in label_counts.keys():
 		prob = label_counts[key] / float(data_size)
 		entropy -= prob * log(prob, 2)
-	return entropy 
+	return entropy
+
+
+def createTree(dataSet, label_names):
+	labels = np.array([row[-1] for row in dataSet])
+	if len(set(labels)) == 1:
+		return labels[0]
+	if len(dataSet[0]) == 1:
+		return majorityCnt(labels)
+	best_feature = choose_best_feature(dataSet)
+	best_feature_label = label_names[best_feature]
+	my_tree = {best_feature_label: {}}
+	best_feature_values = dataSet[:, best_feature]
+	best_feature_values = set(best_feature_values)
+	label_names = del label_names[best_feature]
+	for value in best_feature_values:
+		sub_label_names = label_names[:]
+		my_tree[best_feature_label][value] = createTree(splitData(dataSet, best_feature, value), sub_label_names)
+	return my_tree
+
