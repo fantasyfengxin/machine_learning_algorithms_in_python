f9b4f15c3dcb5a320fd6ed78360d1cfaccada5e6
fx <fantasyfengxin@gmail.com>
2016-07-29 16:53:57 +0800

    finish decision tree

--- CH03/trees.py
+++ CH03/trees.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pickle
 from math import log
 from collections import defaultdict
 
@@ -42,7 +43,7 @@ def splitData(dataSet, index, value):
 	return sub_dataSet
 
 def choose_split_feature(dataSet):
-	dataSet = np.array(dataSet)	
+	dataSet = np.array(dataSet)
 	current_entropy = calcShannonEnt(dataSet)
 	max_information_gain = 0.0
 	split_feature = -1
@@ -61,10 +62,11 @@ def choose_split_feature(dataSet):
 	return split_feature
 
 def createTree(dataSet, label_names):
+	dataSet = np.array(dataSet)
 	labels = np.array([row[-1] for row in dataSet])
 	if len(set(labels)) == 1: # all examples have the same label
 		return labels[0]
-	if len(dataSet[0]) == 1: # no feature to split 
+	if len(dataSet[0]) == 1: # no feature to split
 		return majorityCnt(labels)
 	# get the index of best feature to split
 	split_feature = choose_split_feature(dataSet)
@@ -79,3 +81,21 @@ def createTree(dataSet, label_names):
 		my_tree[split_feature_label][value] = createTree(sub_dataSet, sub_label_names)
 	return my_tree
 
+def classify(input_tree, label_names, test_array):
+	test_array = np.array(test_array)
+	tree_feature_name = list(input_tree.keys())[0]
+	tree_feature_dict = input_tree[tree_feature_name]
+	test_feature_value = str(test_array[label_names.index(tree_feature_name)])
+	tree_feature_value = tree_feature_dict[test_feature_value]
+	if isinstance(tree_feature_value, dict):
+		label = classify(tree_feature_value, label_names,test_array)
+	else: label = tree_feature_value
+	return label
+
+def storeTree(input_tree, filename):
+	with open(filename, 'wb') as file:
+		pickle.dump(input_tree, file)
+
+def grabTree(filename):
+	with open(filename, 'rb') as file:
+		return pickle.load(file)
